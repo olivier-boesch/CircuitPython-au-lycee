d30ee3bfc25c5560ea4f3a0603b590289c9d0fcf
olivier-boesch <olivier.boesch@ac-aix-marseille.fr>
2019-06-01 10:55:06 +0200

    TP ldr ok

--- Application de configuration/configs/photoresistance.py
+++ Application de configuration/configs/photoresistance.py
@@ -1,11 +1,11 @@
 import board
 from analogio import AnalogIn
 import time
-import adafruit_dotstar as dotstar
+from utilities import Notification
 
 # ------------ parametres (pour le professeur) ----------
 Rpont = 10000  # Ohm - résistance du pont
-broche_entree = board.D0  # broche d'entree du potentiomètre
+broche_entree = board.A0  # broche d'entree du potentiomètre
 # -------------------------------------------------------
 
 # ################## MODELE A MODIFIER PAR L'ELEVE ############################
@@ -16,25 +16,18 @@ def modele(R):
 
 # configuration de l'entrée analogique
 entree_analogique = AnalogIn(broche_entree)
-# config de la led interne
-led = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.3)
+# config des notifications
+notif = Notification()
 
 # boucle
 while True:
-    # Flasher la led en vert pour montrer l'instant de la mesure
-    led[0] = (0, 255, 0)
-    time.sleep(0.05)
     # calculer la tension à l'entrée analogique
     tension = entree_analogique.value * 3.3 / 65535
-    print('tension: {:5.3f} V'.format(tension), end='; ')
-    # calculer la résistance du potentiomètre
+    # calculer la résistance de la photorésistance
     r = Rpont * (3.3 / tension - 1)
-    print('valeur resistance: {:6d} Ohms'.format(int(r)), end='; ')
-    # calculer l'angle par le modèle
+    # calculer la luminosité par le modèle
     luminosite = modele(r)
-    # afficher l'angle avec l'unité
-    print('luminosite: {:5d} lx'.format(int(luminosite)))
-    # allumer la led en cyan (attente)
-    led[0] = (0, 150, 255)
+    # afficher les valeurs (lcd ou serie)
+    notif.notify(text='Uldr={:3.2f}V\nr={:3.2e}Ohms\nl={:3.0f}lux')
     # attendre 1s
     time.sleep(1)
